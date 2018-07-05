0437d651d58cc59892882dd693c6cb54c0b7b154
One-Meter-Sunlight <chenkaisupervip@163.com>
2018-07-05 17:34:44 +0800

    third update

--- src/main/java/com/wangbz/examples/sn/core/SequenceService.java
+++ src/main/java/com/wangbz/examples/sn/core/SequenceService.java
@@ -4,12 +4,6 @@ package com.wangbz.examples.sn.core;
  * 序列号服务
  */
 public interface SequenceService {
-    /**
-     * 生成新的订单号
-     *
-     * @param type    订单类型
-     * @param company 公司ID
-     * @return
-     */
+
     String generateOrderNo(String type, String company);
 }
--- src/main/java/com/wangbz/examples/sn/model/CompanySequence.java
+++ src/main/java/com/wangbz/examples/sn/model/CompanySequence.java
@@ -1,13 +1,3 @@
-/*
- * Copyright (c) 2017. 深圳市万连软件有限公司版权所有.
- * 注意:本内容仅限于深圳市万连软件有限公司内部传阅,禁止外泄以及用于其他的商业目的.
- *
- * Document    : CompanySequence.java
- * Created on  : 18-6-23 上午12:12
- * Last Update : 18-6-23 上午12:12
- * Author      : wangbz
- */
-
 package com.wangbz.examples.sn.model;
 
 import lombok.Data;
