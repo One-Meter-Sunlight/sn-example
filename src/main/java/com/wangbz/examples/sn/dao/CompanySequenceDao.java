cf3eb460f4051e2d5246cb5926d4715157955bfa
One-Meter-Sunlight <chenkaisupervip@163.com>
2018-07-05 13:54:12 +0800

    aa

--- src/main/java/com/wangbz/examples/sn/dao/CompanySequenceDao.java
+++ src/main/java/com/wangbz/examples/sn/dao/CompanySequenceDao.java
@@ -5,12 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CompanySequenceDao extends JpaRepository<CompanySequence, String> {
 
-    /**
-     * 查询规则序列表信息
-     *
-     * @param type
-     * @param company
-     * @return
-     */
     CompanySequence findByTypeAndCompany(String type, String company);
 }
